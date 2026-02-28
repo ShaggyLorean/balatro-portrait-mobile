b5c462544509884b7dbb19d5ff03ebc7396cc31e
KtourzaJeremy <jeremyktourza@gmail.com>
2026-02-28 10:08:30 +0100

    Inventory in portrait mode
    The card inventory (by clicking on the deck in game) is now adapted to vertical mode

--- src/functions/UI_definitions.lua
+++ src/functions/UI_definitions.lua
@@ -3379,6 +3379,11 @@ function G.UIDEF.current_stake()
 end
 
 function G.UIDEF.view_deck(unplayed_only)
+  local SCALE =  1
+  if G.F_PORTRAIT then
+    SCALE = 0.8
+  end
+
   local deck_tables = {}
   remove_nils(G.playing_cards)
   G.VIEWING_DECK = true
@@ -3397,9 +3402,9 @@ function G.UIDEF.view_deck(unplayed_only)
     if SUITS[suit_map[j]][1] then
       local view_deck = CardArea(
         G.ROOM.T.x + 0.2*G.ROOM.T.w/2,G.ROOM.T.h,
-        6.5*G.CARD_W,
-        0.6*G.CARD_H,
-        {card_limit = #SUITS[suit_map[j]], type = 'title', view_deck = true, highlight_limit = 0, card_w = G.CARD_W*0.7, draw_layers = {'card'}})
+        6.5*G.CARD_W*SCALE,
+        0.6*G.CARD_H*SCALE,
+        {card_limit = #SUITS[suit_map[j]], type = 'title', view_deck = true, highlight_limit = 0, card_w = G.CARD_W*0.7*SCALE, draw_layers = {'card'}})
       table.insert(deck_tables, 
       {n=G.UIT.R, config={align = "cm", padding = 0}, nodes={
         {n=G.UIT.O, config={object = view_deck}}
@@ -3490,7 +3495,83 @@ function G.UIDEF.view_deck(unplayed_only)
     }}
   end
 
+  local rank_rows = {n = G.UIT.R, config = {align = "cm", padding = 0.05}, nodes = {}}
+  for i = 13, 1, -1 do
+    local mod_delta2 = mod_rank_tallies[i] ~= rank_tallies[i]
+    rank_rows.nodes[#rank_rows.nodes+1] = {n=G.UIT.C, config={align = "cm", padding = 0.07}, nodes={
+      -- Rank row (A, K, Q, etc.)
+      {n=G.UIT.R, config={align = "cm", r = 0.1, padding = 0.04, emboss = 0.04, minw = 0.5, colour = G.C.L_BLACK}, nodes={
+        {n=G.UIT.T, config={text = rank_name_mapping[i],colour = G.C.JOKER_GREY, scale = 0.35, shadow = true}},
+      }},
+      -- Tally row
+      {n=G.UIT.R, config={align = "cm", minw = 0.4}, nodes={
+        mod_delta2 and {n=G.UIT.O, config={object = DynaText({string = {{string = ''..rank_tallies[i], colour = flip_col},{string =''..mod_rank_tallies[i], colour = G.C.BLUE}}, colours = {G.C.RED}, scale = 0.4, y_offset = -2, silent = true, shadow = true, pop_in_rate = 10, pop_delay = 4})}} or
+        {n=G.UIT.T, config={text = rank_tallies[i] or 'NIL',colour = flip_col, scale = 0.45, shadow = true}}
+      }}
+    }}
+  end
 
+  if G.F_PORTRAIT then
+    local t = 
+    {n=G.UIT.ROOT, config={align = "cm", colour = G.C.CLEAR}, nodes={
+      {n=G.UIT.R, config={align = "cm", padding = 0.05}, nodes={}},
+      {n=G.UIT.R, config={align = "cm"}, nodes={
+        {n=G.UIT.R, config={align = "cm", minw = 1.5, minh = 2, r = 0.1, colour = G.C.BLACK, emboss = 0.05}, nodes={
+          {n=G.UIT.R, config={align = "cm", padding = 0.1}, nodes={
+            {n=G.UIT.C, config={align = "cm", r = 0.1, colour = G.C.L_BLACK, emboss = 0.05, padding = 0.15}, nodes={
+              {n=G.UIT.R, config={align = "cm"}, nodes={
+                {n=G.UIT.O, config={object = DynaText({string = G.GAME.selected_back.loc_name, colours = {G.C.WHITE}, bump = true, rotate = true, shadow = true, scale = 0.6 - string.len(G.GAME.selected_back.loc_name)*0.01})}},
+              }},
+              {n=G.UIT.R, config={align = "cm", r = 0.1, padding = 0.1, minw = 2.5, minh = 1.3, colour = G.C.WHITE, emboss = 0.05}, nodes={
+                {n=G.UIT.O, config={object = UIBox{
+                  definition = G.GAME.selected_back:generate_UI(nil,0.7, 0.5, G.GAME.challenge),
+                  config = {offset = {x=0,y=0}}
+                }}}
+              }}
+            }},
+            {n=G.UIT.B, config={w = 0.25, h = 0.25}},
+            {n=G.UIT.C, config={align = "cm", r = 0.1, outline_colour = G.C.L_BLACK, line_emboss = 0.05, outline = 1.5}, nodes={
+              {n=G.UIT.R, config={align = "cm", minh = 0.05, padding = 0.07}, nodes={
+                  {n=G.UIT.O, config={object = DynaText({string = {{string = localize('k_base_cards'), colour = G.C.RED}, modded and {string = localize('k_effective'), colour = G.C.BLUE} or nil}, colours = {G.C.RED}, silent = true,scale = 0.4,pop_in_rate = 10, pop_delay = 4})}}
+              }},
+              {n=G.UIT.R, config={align = "cm", minh = 0.05, padding = 0.1}, nodes={
+                tally_sprite({x=1,y=0},{{string = ''..ace_tally, colour = flip_col},{string =''..mod_ace_tally, colour = G.C.BLUE}}, {localize('k_aces')}),--Aces
+                tally_sprite({x=2,y=0},{{string = ''..face_tally, colour = flip_col},{string =''..mod_face_tally, colour = G.C.BLUE}}, {localize('k_face_cards')}),--Face
+                tally_sprite({x=3,y=0},{{string = ''..num_tally, colour = flip_col},{string =''..mod_num_tally, colour = G.C.BLUE}}, {localize('k_numbered_cards')}),--Numbers
+              }},
+              {n=G.UIT.R, config={align = "cm", minh = 0.05, padding = 0.1}, nodes={
+                tally_sprite({x=3,y=1}, {{string = ''..suit_tallies['Spades'], colour = flip_col},{string =''..mod_suit_tallies['Spades'], colour = G.C.BLUE}}, {localize('Spades', 'suits_plural')}),
+                tally_sprite({x=0,y=1}, {{string = ''..suit_tallies['Hearts'], colour = flip_col},{string =''..mod_suit_tallies['Hearts'], colour = G.C.BLUE}}, {localize('Hearts', 'suits_plural')}),
+                tally_sprite({x=2,y=1}, {{string = ''..suit_tallies['Clubs'], colour = flip_col},{string =''..mod_suit_tallies['Clubs'], colour = G.C.BLUE}}, {localize('Clubs', 'suits_plural')}),
+                tally_sprite({x=1,y=1}, {{string = ''..suit_tallies['Diamonds'], colour = flip_col},{string =''..mod_suit_tallies['Diamonds'], colour = G.C.BLUE}}, {localize('Diamonds', 'suits_plural')}),
+              }},
+            }}
+          }},
+          rank_rows
+        }}
+      }},
+      {n=G.UIT.R, config={align = "cm"}, nodes={
+        {n=G.UIT.B, config={w = 0.2, h = 0.8}},
+      }},
+      {n=G.UIT.R, config={align = "cm"}, nodes={
+        {n=G.UIT.B, config={w = 0.2, h = 0.5}},
+        {n=G.UIT.C, config={align = "cm", padding = 0.1, r = 0.1, colour = G.C.BLACK, emboss = 0.05}, nodes=deck_tables}
+      }},
+      {n=G.UIT.R, config={align = "cm", minh = 0.8, padding = 0.05}, nodes={
+        modded and {n=G.UIT.R, config={align = "cm"}, nodes={
+          {n=G.UIT.C, config={padding = 0.3, r = 0.1, colour = mix_colours(G.C.BLUE, G.C.WHITE,0.7)}, nodes = {}},
+          {n=G.UIT.T, config={text =' '..localize('ph_deck_preview_effective'),colour = G.C.WHITE, scale =0.3}},
+        }} or nil,
+        wheel_flipped > 0 and {n=G.UIT.R, config={align = "cm"}, nodes={
+          {n=G.UIT.C, config={padding = 0.3, r = 0.1, colour = flip_col}, nodes = {}},
+          {n=G.UIT.T, config={text =' '..(wheel_flipped > 1 and
+            localize{type = 'variable', key = 'deck_preview_wheel_plural', vars = {wheel_flipped}} or
+            localize{type = 'variable', key = 'deck_preview_wheel_singular', vars = {wheel_flipped}}),colour = G.C.WHITE, scale =0.3}},
+        }} or nil,
+      }}
+    }}
+    return t
+  else
   local t = 
   {n=G.UIT.ROOT, config={align = "cm", colour = G.C.CLEAR}, nodes={
     {n=G.UIT.R, config={align = "cm", padding = 0.05}, nodes={}},
@@ -3547,6 +3628,7 @@ function G.UIDEF.view_deck(unplayed_only)
     }}
   }}
   return t
+end
 end
 
 function tally_sprite(pos, value, tooltip)
